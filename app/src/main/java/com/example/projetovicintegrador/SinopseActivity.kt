986b1d28d7b4678a6e2a2f818b89fc6ee3e198d9
Rayssa <rayssav.ss@gmail.com>
2021-07-08 22:44:46 -0300

    atualizacao de xml sinopse e etc

--- app/src/main/java/com/example/projetovicintegrador/SinopseActivity.kt
+++ app/src/main/java/com/example/projetovicintegrador/SinopseActivity.kt
@@ -1,14 +1,13 @@
 package com.example.projetovicintegrador
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.sinopse.*
 
-class MainActivity : AppCompatActivity() {
+class SinopseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val intent = Intent(this, SinopseActivity::class.java)
-        startActivity(intent)
+        setContentView(R.layout.sinopse)
+    cardView.setBackgroundResource(R.drawable.bg_left_rounded)
     }
 }
